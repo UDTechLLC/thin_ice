dd2927b49104c25b708e949b69f415754ec31365
Dmitry <dshtorm@gmail.com>
2016-03-07 00:01:16 +0200

    init information list

--- ThinIce/ThinIce/InformationViewController.m
+++ ThinIce/ThinIce/InformationViewController.m
@@ -10,13 +10,48 @@
 
 @interface InformationViewController ()
 
-@property (weak, nonatomic) IBOutlet UIScrollView *scrollForInformationBlock;
-@property (weak, nonatomic) IBOutlet UIView *backgroundForInformationFields;
+@property (weak, nonatomic) IBOutlet UIScrollView                       *scrollForInformationBlock;
+@property (weak, nonatomic) IBOutlet UIView                             *backgroundForInformationFields;
 
+// FirstName Block
+@property (weak, nonatomic) IBOutlet UILabel                            *firstNameLabel;
+@property (weak, nonatomic) IBOutlet UITextField                        *firstNameTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *firstNameBorderLine;
 
+// LastName Block
+@property (weak, nonatomic) IBOutlet UILabel                            *LastNameLabel;
+@property (weak, nonatomic) IBOutlet UITextField                        *LastNameTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *LastNameBorderLine;
 
+// Choose Sex
+@property (weak, nonatomic) IBOutlet UILabel                            *chooseSexLabel;
+@property (weak, nonatomic) IBOutlet UITextField                        *chooseSexTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *chooseSexBorderLine;
 
+// DateOfBirth Block
+@property (weak, nonatomic) IBOutlet UILabel                            *dateOfBirthLabel;
+@property (weak, nonatomic) IBOutlet UITextField                        *dayOfBirthTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *dayOfBirthBorderLine;
 
+@property (weak, nonatomic) IBOutlet UITextField                        *monthOfBirthTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *monthOfBirthBorderLine;
+
+@property (weak, nonatomic) IBOutlet UITextField                        *yearTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *yearBorderLine;
+
+// Height Block
+@property (weak, nonatomic) IBOutlet UILabel                            *heightLabel;
+@property (weak, nonatomic) IBOutlet UITextField                        *heightTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *heightBorderLine;
+
+// Width Block
+@property (weak, nonatomic) IBOutlet UILabel                            *widthLabel;
+@property (weak, nonatomic) IBOutlet UITextField                        *widthTextField;
+@property (weak, nonatomic) IBOutlet UIView                             *widthBorderLine;
+
+// Save Button
+
+@property (weak, nonatomic) IBOutlet UIButton                           *saveButton;
 
 @end
 
@@ -33,13 +68,51 @@
     
     self.view.backgroundColor = [UIColor clearColor];
     
-    self.scrollForInformationBlock.layer.cornerRadius = 13;
-    self.scrollForInformationBlock.backgroundColor = [[HelperManager sharedServer] colorwithHexString:@"#346b7d" alpha: 0.5];
+    self.backgroundForInformationFields.layer.cornerRadius = 13;
+    self.backgroundForInformationFields.backgroundColor = [[HelperManager sharedServer] colorwithHexString:@"#346b7d" alpha: 0.5];
     
+// FirstName Block init
+    self.firstNameLabel.textColor = [UIColor lightGrayColor];
+    self.firstNameTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.firstNameBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
     
+// LastName Block init
+    self.LastNameLabel.textColor = [UIColor lightGrayColor];
+    self.LastNameTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.LastNameBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
     
+// ChooseSex Block init
+    self.chooseSexLabel.textColor = [UIColor lightGrayColor];
+    self.chooseSexTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.chooseSexBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
     
+// DateOfBirth Block init
+    self.dateOfBirthLabel.textColor = [UIColor lightGrayColor];
     
+    self.dayOfBirthTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.dayOfBirthBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
+    
+    self.monthOfBirthTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.monthOfBirthBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
+    
+    self.yearTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.yearBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
+    
+// Height Block init
+    self.heightLabel.textColor = [UIColor lightGrayColor];
+    self.heightTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.heightBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
+    
+// Width Block init
+    self.widthLabel.textColor = [UIColor lightGrayColor];
+    self.widthTextField.textColor = [[HelperManager sharedServer] colorwithHexString:ColorFromPlaceHolderText alpha:1.0];
+    self.widthBorderLine.backgroundColor = [[HelperManager sharedServer] colorwithHexString:ColorFromSeparators alpha:1.0];
+    
+// Create SaveButton
+    self.saveButton.layer.cornerRadius = 13;
+    self.saveButton.backgroundColor = [[HelperManager sharedServer] colorwithHexString:@"#33c6cb" alpha:1.0];
+    self.saveButton.tintColor = [[HelperManager sharedServer] colorwithHexString:@"#ffffff" alpha:1.0];
+    [self.saveButton setTitle:@"Save" forState:UIControlStateNormal];
 }
 
 @end
